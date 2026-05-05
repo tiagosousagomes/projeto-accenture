bb9e4862dac2e8a1171326f22ede1acbc625de1b
Tiago Sousa <tiagosousagomes14@gmail.com>
2026-05-04 21:53:37 -0300

    configurando projeto com spring boot

---
+++ academiaJava/src/main/java/accentura/brasil/academiaJava/AcademiaJavaApplication.java
@@ -0,0 +1,14 @@
+package accentura.brasil.academiaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaJavaApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ academiaJava/src/test/java/accentura/brasil/academiaJava/AcademiaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package accentura.brasil.academiaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
